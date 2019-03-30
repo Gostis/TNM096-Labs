bc5743e024afaa05b37d31fda0a17d852ca4999d
Gostis <jacob.e.nyman@gmail.com>
2019-03-30 12:26:57 +0100

    Adding Lab1 to rep

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,20 @@
+// Lab1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	// open [] minsta elementet måste vara soterat men inte resten log n att sortera.
+	// closed [] bara lägga till
+	
+	int test;
+	cout << "Hello world";
+	cin >> test;
+	return 0;
+}
+
